f9186810024dc2728a388f68ff436c105634b4ce
amir8102 <amirh312@gmail.com>
2018-12-06 15:16:24 +0200

    commit

---
+++ app_/login/loginCtrl.js
@@ -0,0 +1,20 @@
+
+app.controller("loginCtrl", function($scope, $location, user) {
+    
+    $scope.email = "amirh312@gmail.com";
+    $scope.pwd = "123";
+
+    $scope.invalidLogin = false;
+
+    $scope.login = function() {
+        $scope.invalidLogin = false;
+
+        user.login($scope.email, $scope.pwd).then(function() {
+            // success login
+            $location.path("/Homeowner")
+        }, function(error) {
+            // failed login
+            $scope.invalidLogin = true;
+        })
+    }
+});
---
+++ app_/login/loginDir.js
@@ -0,0 +1,6 @@
+// app.directive("loginDir", function() {
+//     return {
+//         templateUrl: "app/login/login.html",
+//         controller: "loginCtrl"
+//     }
+// })
